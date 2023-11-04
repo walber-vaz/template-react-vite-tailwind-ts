21db9d4d95b78a5905a71eb297eaddba279ff095
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-11-04 10:30:39 -0300

    feat: refactor social media links and add license information

--- src/App.tsx
+++ src/App.tsx
@@ -29,25 +29,62 @@ export const App = (): React.JSX.Element => {
       >
         Template 2k
       </h1>
-      <div className="flex items-center justify-center">
-        <img src={linkedin} alt="linkedin" className="mr-2 h-8 w-8" />
-        <a
-          href="https://linkedin.com/in/walber-vaz"
-          target="_blank"
-          rel="noreferrer"
-          className="text-center text-2xl"
+      <div className="flex flex-col justify-center p-4 md:flex-row md:items-center">
+        <div
+          className="
+            flex
+            items-center
+            justify-center
+          "
         >
-          Linkind In Profile (Walber Vaz)
-        </a>
-        <img src={github} alt="github" className="ml-2 h-8 w-8" />
-        <a
-          href="https://github.com/walber-vaz"
-          target="_blank"
-          rel="noreferrer"
-          className="text-center text-2xl"
+          <img src={linkedin} alt="linkedin" className="h-8 w-8" />
+          <a
+            href="https://linkedin.com/in/walber-vaz"
+            target="_blank"
+            rel="noreferrer"
+            className="ml-2 text-2xl"
+          >
+            Linkind (Walber Vaz)
+          </a>
+        </div>
+        <div
+          className="
+            flex
+            items-center
+            justify-center
+          "
         >
-          Github Profile (Walber Vaz)
-        </a>
+          <img src={github} alt="github" className="h-10 w-10" />
+          <a
+            href="https://github.com/walber-vaz"
+            target="_blank"
+            rel="noreferrer"
+            className="ml-2 text-2xl"
+          >
+            Github (Walber Vaz)
+          </a>
+        </div>
+      </div>
+      <div
+        className="
+            flex
+            items-center
+            justify-center
+            md:items-center
+            md:justify-center
+          "
+      >
+        <p className="text-xl md:text-2xl">
+          Licença
+          <a
+            href="https://github.com/walber-vaz/template-react-vite-tailwind-ts/blob/main/LICENSE"
+            target="_blank"
+            rel="noreferrer"
+            className="ml-2 text-xl md:text-2xl"
+          >
+            MIT
+          </a>
+        </p>
       </div>
     </section>
   );
